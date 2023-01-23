320617877971e7df8b2c93481b3cf981212431ed
professional-pigeon <kyle.kayperez@gmail.com>
2023-01-23 11:45:27 -0800

    Add locations model, migrate schema

---
+++ db/migrate/20230123194240_create_locations.rb
@@ -0,0 +1,10 @@
+class CreateLocations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :locations do |t|
+      t.string :name, null: false
+      t.string :type, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_20_191611) do
+ActiveRecord::Schema.define(version: 2023_01_23_194240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "type", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "user_name", null: false
