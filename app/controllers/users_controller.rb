d8b88c6dd53285b17c1df1d240d41ce62608e425
professional-pigeon <kyle.kayperez@gmail.com>
2023-02-08 23:16:10 -0800

    add create and outline log in in users controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(params[:user])
+    @user.password = params[:password]
+    @user.save!
   end
+
+  # def login
+  #   @user = User.find_by_email(params[:email])
+  #   if @user.password == params[:password]
+  #     give_token
+  #   else
+  #     redirect_to home_url
+  #   end
+  # end
 
   def destroy
   end
