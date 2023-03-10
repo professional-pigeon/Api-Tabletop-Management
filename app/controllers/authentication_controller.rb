de97a7ef2ff8a24052f1b673c9dacaf591b893d8
professional-pigeon <kyle.kayperez@gmail.com>
2023-03-10 11:45:00 -0800

    update rack cors config for localhost 4000 origin

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -6,7 +6,7 @@ class AuthenticationController < ApplicationController
     @user = User.find_by_user_name(params[:user_name])
     if @user&.authenticate(params[:password])
       session[:user_id] = @user.id
-      render json: { user_name: @user.user_name }, status: :ok
+      render json: @user, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
